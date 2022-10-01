7a379a0612f7280d766acdf2c4eb5468b22aeceb
Gustavo Jannuzzi <80784697+GustavoJannuzzi@users.noreply.github.com>
2022-10-01 17:25:34 -0300

    Add files via upload

---
+++ quantamental_app.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from scipy import stats 
+import yfinance as yf
+import streamlit as st
+
+# função para converter valores percentuais para valores decimais
+def convert_perc(value):
+    return pd.to_numeric(value.replace('%','').replace('.', '').replace(',', '.'))
+
+# mapeando colunas que possuem valores percentuais
+convs = {5: convert_perc, 12: convert_perc, 13: convert_perc, 15: convert_perc, 16: convert_perc, 20: convert_perc}
+
+url = 'http://fundamentus.com.br/resultado.php'
+agent = {"User-Agent":"Mozzila/5.0"}
+resposta = requests.get(url, headers = agent)
+soup = BeautifulSoup(resposta.text, 'lxml')
+tabela = soup.find_all('table')[0]
+df = pd.read_html(str(tabela), decimal = ',', thousands='.', converters=convs)[0]
+
+fundamentus = df
+
+#Data cleaning
+fundamentus[fundamentus.isnull().any(axis=1)]
+
+for column in ['P/L', 'P/VP','PSR',  'EV/EBITDA']:
+    fundamentus[column].fillna(fundamentus[column].mean(), inplace = True)
+
+fundamentus = fundamentus.drop( ['Div.Yield', 'P/Ativo','P/Cap.Giro', 'P/EBIT', 'P/Ativ Circ.Liq', 'EV/EBIT',
+                                'Mrg Ebit', 'Mrg. Líq.', 'Liq. Corr.', 'ROIC', 'ROE', 
+                                'Liq.2meses','Patrim. Líq', 'Dív.Brut/ Patrim.', 'Cresc. Rec.5a'], axis=1)
+
+
+## Calculando percentual dos indicadore
+metrics = {
+            'P/L': 'P/L Percentile',
+            'P/VP':'P/VP Percentile',
+            'PSR': 'PSR Percentile',
+            'EV/EBITDA':'EV/EBITDA Percentile'
+}
+
+for row in fundamentus.index:
+    for metric in metrics.keys():
+        fundamentus.loc[row, metrics[metric]] = stats.percentileofscore(fundamentus[metric], fundamentus.loc[row, metric])/100
+
+# Printa cada percentual para garantir que esta sendo calculado
+for metric in metrics.values():
+    print(fundamentus[metric])
+
+
+
+# STREAMLIT 
+
+st.title("Estratégia Quantamental")
+
+ticker = fundamentus['Papel']+'.SA'
+ticker = ticker.tolist()
+ticker = st.selectbox('Escolha um ativo', ticker)
